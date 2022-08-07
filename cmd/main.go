563d24fefd922e38fd9b98b24adadc7d89cffe2c
AJT <alextrelore@gmail.com>
2022-08-07 11:09:08 +0100

    add stars/recent activity

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,104 @@
 package main
 
 import (
-	"io/ioutil"
+	"context"
+	"log"
+	"os"
+	"strings"
+	"text/template"
+
+	graphql "github.com/hasura/go-graphql-client"
+	"golang.org/x/oauth2"
 )
 
-func main() {
-	f, err := ioutil.ReadFile("README.md")
-	if err != nil {
-		panic(err)
+type readmeData struct {
+	BlogPostTitles []string
+	RecentStars    []struct {
+		Description   string `json:"description"`
+		NameWithOwner string `json:"nameWithOwner"`
 	}
-	println(string(f))
+	RecentActivity []struct {
+		Description string `json:"description"`
+		Name        string `json:"name"`
+	}
+}
+
+var readme = strings.ReplaceAll(`### Hi there 👋
+
+---
+
+<a href="https://github.com/trelore"><img src="https://img.shields.io/github/followers/trelore.svg?label=GitHub&style=social" alt="GitHub"></a>
+<a href="https://twitter.com/AlexanderJophus"><img src="https://img.shields.io/twitter/follow/AlexanderJophus?label=Twitter&style=social" alt="Twitter"></a>
+<a href="https://twitch.tv/dejophus"><img src="https://img.shields.io/twitch/status/dejophus?style=social" alt="Twitch"></a>
+
+§§§yaml
+name: Alexander Jophus
+located_in: Bristol, UK
+job: Senior Software Engineer (Go)
+
+fields_of_interests: ["Developer Experience", "DevOps", "Making Microservices Go Zoom"]
+familiarity: ["Go", "Python", "Kubernetes"]
+currently_learning: ["Game Design", "Elixir"]
+hobbies: ["Gaming", "Music"]
+§§§
+
+<a href="https://github.com/trelore/trelore">
+  <img align="center" src="https://github-readme-stats.vercel.app/api/top-langs/?username=trelore&hide=java,html,tex&langs_count=3&theme=vision-friendly-dark" />
+</a>
+<a href="https://github.com/trelore/trelore">
+  <img align="center" src="https://github-readme-stats.vercel.app/api?username=trelore&show_icons=true&line_height=27&count_private=true&theme=vision-friendly-dark" alt="Alexanders GitHub Stats" />
+</a>
+
+## Recent Stars
+| Repository | Description |
+|---|---|{{ range .RecentStars }}
+| [{{ .NameWithOwner }}](https://www.github.com/{{ .NameWithOwner }}) | {{ .Description }} |{{ end }}
+
+## Actively Working On
+| Repository | Description |
+|---|---|{{ range .RecentActivity }}
+| [{{ .Name }}](https://www.github.com/trelore/{{ .Name }}) | {{ .Description }} |{{ end }}`, "§", "`")
+
+var query struct {
+	User struct {
+		StarredRepositories struct {
+			Nodes []struct {
+				Description   string `json:"description"`
+				NameWithOwner string `json:"nameWithOwner"`
+			} `json:"nodes"`
+		} `graphql:"starredRepositories(last:5)"`
+		Repositories struct {
+			Nodes []struct {
+				Description string `json:"description"`
+				Name        string `json:"name"`
+			} `json:"nodes"`
+		} `graphql:"repositories(privacy:PUBLIC, last:5, orderBy:{field:PUSHED_AT, direction:ASC})"`
+	} `graphql:"user(login: \"trelore\")"`
+}
+
+func main() {
+	tmpl := template.Must(template.New("README").Parse(readme))
+
+	out, err := os.OpenFile("README.md", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out.Close()
+
+	src := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv("GITHUB_GRAPHQL_TOKEN")},
+	)
+	httpClient := oauth2.NewClient(context.Background(), src)
+
+	client := graphql.NewClient("https://api.github.com/graphql", httpClient)
+	err = client.Query(context.Background(), &query, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tmpl.Execute(out, readmeData{
+		BlogPostTitles: []string{},
+		RecentStars:    query.User.StarredRepositories.Nodes,
+		RecentActivity: query.User.Repositories.Nodes,
+	})
 }
---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/trelore/trelore
+
+go 1.18
+
+require (
+	github.com/hasura/go-graphql-client v0.7.2
+	golang.org/x/oauth2 v0.0.0-20220722155238-128564f6959c
+)
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/klauspost/compress v1.10.3 // indirect
+	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+	nhooyr.io/websocket v1.8.7 // indirect
+)
