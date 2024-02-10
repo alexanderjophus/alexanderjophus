f8b400ea60a01d7b4632f705f8f777316db3184c
AJT <alextrelore@gmail.com>
2024-02-10 09:31:43 +0000

    move to CUE (for some reason)

--- cmd/main.go
+++ cmd/main.go
@@ -2,18 +2,30 @@ package main
 
 import (
 	"context"
+	_ "embed"
 	"log"
 	"os"
-	"strings"
 	"text/template"
 
+	"cuelang.org/go/cue"
+	"cuelang.org/go/cue/cuecontext"
+	"cuelang.org/go/pkg/encoding/yaml"
 	graphql "github.com/hasura/go-graphql-client"
 	"golang.org/x/oauth2"
 )
 
+//go:embed README.tpl
+var readme string
+
+//go:embed main_go_gen.cue
+var cuefile string
+
+//go:embed about_me.cue
+var aboutMe string
+
 type readmeData struct {
-	BlogPostTitles []string
-	RecentStars    []struct {
+	AboutMe     string
+	RecentStars []struct {
 		Description   string `json:"description"`
 		NameWithOwner string `json:"nameWithOwner"`
 	}
@@ -23,60 +35,16 @@ type readmeData struct {
 	}
 }
 
-var readme = strings.ReplaceAll(`### Hi there 👋
+type AboutMe struct {
+	Name     string `json:"name"`
+	Location string `json:"location"`
+	Job      string `json:"job"`
 
----
-
-<a href="https://github.com/alexanderjophus"><img src="https://img.shields.io/github/followers/alexanderjophus.svg?label=GitHub&style=social" alt="GitHub"></a>
-<a href="https://twitter.com/AlexanderJophus"><img src="https://img.shields.io/twitter/follow/AlexanderJophus?label=Twitter&style=social" alt="Twitter"></a>
-<a href="https://twitch.tv/dejophus"><img src="https://img.shields.io/twitch/status/dejophus?style=social" alt="Twitch"></a>
-
-§§§yaml
-name: Alexander Jophus
-located_in: Bristol, UK
-job: Senior Software Engineer
-
-fields_of_interests: ["Developer Experience", "DevOps", "User Experience"]
-familiarity: ["Go", "Rust", "Python", "Kubernetes"]
-currently_learning: ["Game Design", "Elixir", "Rust/Bevy"]
-hobbies: ["Gaming", "Music", "Bouldering"]
-§§§
-
-<a href="https://github.com/alexanderjophus/alexanderjophus">
-  <img align="center" src="https://github-readme-stats-git-masterrstaa-rickstaa.vercel.app/api/top-langs?username=alexanderjophus&hide=java,html,tex&langs_count=3&theme=vision-friendly-dark" />
-</a>
-<a href="https://github.com/alexanderjophus/alexanderjophus">
-  <img align="center" src="https://github-readme-stats-git-masterrstaa-rickstaa.vercel.app/api?username=alexanderjophus&show_icons=true&line_height=27&count_private=true&theme=vision-friendly-dark" alt="Alexanders GitHub Stats" />
-</a>
-
-[![GitHub Streak](http://github-readme-streak-stats.herokuapp.com?user=alexanderjophus&theme=dark&hide_border=true)](https://git.io/streak-stats)
-
-## Familiarity
-<div class="row">
-  <img height="100" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/go/go-original-wordmark.svg" />
-  <img height="100" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rust/rust-plain.svg" />
-  <img height="100" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />
-  <img height="100" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/googlecloud/googlecloud-original.svg" />
-  <img height="100" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-original.svg" />
-  <img height="100" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kubernetes/kubernetes-plain.svg" />
-  <img height="100" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" />
-  <img height="100" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />
-  <img height="100" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" />
-  <img height="100" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/hugo/hugo-original.svg" />
-  <img height="100" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/latex/latex-original.svg" />
-  <img height="100" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/terraform/terraform-original.svg" />
-  <img height="100" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/blender/blender-original.svg" />
-</div>
-
-## Recent Stars
-| Repository | Description |
-|---|---|{{ range .RecentStars }}
-| [{{ .NameWithOwner }}](https://www.github.com/{{ .NameWithOwner }}) | {{ .Description }} |{{ end }}
-
-## Actively Working On (publicly)
-| Repository | Description |
-|---|---|{{ range .RecentActivity }}
-| [{{ .Name }}](https://www.github.com/alexanderjophus/{{ .Name }}) | {{ .Description }} |{{ end }}`, "§", "`")
+	FieldsOfInterest  []string `json:"fieldsOfInterest"`
+	Hobbies           []string `json:"hobbies"`
+	Familiarities     []string `json:"familiarities"`
+	CurrentlyLearning []string `json:"currentlyLearning"`
+}
 
 var query struct {
 	User struct {
@@ -96,6 +64,27 @@ var query struct {
 }
 
 func main() {
+	ctx := cuecontext.New()
+	v := ctx.CompileString(cuefile)
+	if v.Err() != nil {
+		log.Fatal(v.Err())
+	}
+
+	aboutMe := ctx.CompileString(aboutMe)
+	if aboutMe.Err() != nil {
+		log.Fatal(aboutMe.Err())
+	}
+
+	v = v.Unify(aboutMe).LookupPath(cue.ParsePath("#AboutMe"))
+	if v.Err() != nil {
+		log.Fatal(v.Err())
+	}
+
+	yaml, err := yaml.Marshal(v)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	tmpl := template.Must(template.New("README").Parse(readme))
 
 	out, err := os.OpenFile("README.md", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
@@ -116,7 +105,7 @@ func main() {
 	}
 
 	tmpl.Execute(out, readmeData{
-		BlogPostTitles: []string{},
+		AboutMe:        yaml,
 		RecentStars:    query.User.StarredRepositories.Nodes,
 		RecentActivity: query.User.Repositories.Nodes,
 	})
--- go.mod
+++ go.mod
@@ -1,18 +1,22 @@
 module github.com/trelore/trelore
 
-go 1.18
+go 1.21
 
 require (
-	github.com/hasura/go-graphql-client v0.7.2
-	golang.org/x/oauth2 v0.0.0-20220722155238-128564f6959c
+	cuelang.org/go v0.7.0
+	github.com/hasura/go-graphql-client v0.11.0
+	golang.org/x/oauth2 v0.17.0
 )
 
 require (
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/google/uuid v1.3.0 // indirect
-	github.com/klauspost/compress v1.10.3 // indirect
-	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
+	github.com/cockroachdb/apd/v3 v3.2.1 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/uuid v1.5.0 // indirect
+	github.com/mpvl/unique v0.0.0-20150818121801-cbe035fff7de // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
-	nhooyr.io/websocket v1.8.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	nhooyr.io/websocket v1.8.10 // indirect
 )
